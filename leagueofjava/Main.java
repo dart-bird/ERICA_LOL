36cdb6e0ab989c6d029fc41015002936b46899b6
krumaska <okokrt@gmail.com>
2019-11-28 23:13:37 +0900

    Update Main.java issue closed

--- leagueofjava/Main.java
+++ leagueofjava/Main.java
@@ -6,7 +6,7 @@ import java.io.File;
 import ClassSet.Content.UserContent;
 public class Main {
     public static void main(String[] args){
-        File userFile = new File("../content/data/userdata.txt");
+        File userFile = new File("../leagueofjava/content/data/userdata.txt");
         UserContent user1 = new UserContent(userFile);
         user1.loadUserList();
         System.out.println(user1.getUserList());
