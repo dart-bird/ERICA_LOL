545e8fcf1143a72b621903170ec8880c097dd81a
krumaska <okokrt@gmail.com>
2019-11-28 14:26:47 +0900

    Update RW.java 3 func & repackaging

---
+++ leagueofjava/ClassSet/ReadWriter/RW.java
@@ -0,0 +1,40 @@
+package ClassSet.ReadWriter;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.File;
+import Interface.IFRW;
+
+public class RW implements IFRW{
+    File file;
+    private String ContentList = "";
+
+    public RW(File file) {
+        this.file = file;
+        ContentList = "";
+    }
+    
+    public void loadContent(File file){
+        try {
+            Scanner scan = new Scanner(file);
+            while (scan.hasNextLine()){
+                ContentList += scan.nextLine();
+                ContentList += "\n";
+            }
+            scan.close();
+
+        } catch (FileNotFoundException e ) {
+            // TODO: handle exception
+        }
+    }
+    
+    @Override
+    public String getContent() {
+        return ContentList;
+    }
+
+    @Override
+    public void writeContent(File file, String inContentString) {
+        // TODO Auto-generated method stub
+        
+    }
+}
