dba2ab4c82464791520255b359fcfbee7f5e9170
krumaska <okokrt@gmail.com>
2019-11-28 14:24:45 +0900

    Update IFRW.java 2 func & repackage

---
+++ leagueofjava/Interface/IFRW.java
@@ -0,0 +1,7 @@
+package Interface;
+import java.io.File;
+
+public interface IFRW {
+    abstract String getContent();//상점, 마이페이지, 인게임 정보 가져오는데 활용
+    abstract void writeContent(File file, String inContentString);
+}
