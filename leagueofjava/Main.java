6423774f86304c1644529a4d3699dba129413498
krumaska <okokrt@gmail.com>
2019-12-03 00:41:34 +0900

    Update Main due to other class

--- leagueofjava/Main.java
+++ leagueofjava/Main.java
@@ -6,9 +6,11 @@ import java.io.File;
 import ClassSet.Content.UserContent;
 public class Main {
     public static void main(String[] args){
-        File userFile = new File("../leagueofjava/content/data/userdata.txt");
+        File userFile = new File("../leagueofjava/content/data/userdata.gdata");
         UserContent user1 = new UserContent(userFile);
         user1.loadUserList();
         System.out.println(user1.getUserList());
+        user1.loadDB();
+        System.out.println(user1.getUserData("gold"));
     }
 }
