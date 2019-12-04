c03bafc847db0a98b5e2b88c34d100e0d6b6de5a
krumaska <okokrt@gmail.com>
2019-12-05 02:16:55 +0900

    data random in test

--- leagueofjava/Main.java
+++ leagueofjava/Main.java
@@ -2,10 +2,13 @@ import Interface.IFRW;
 import ClassSet.ReadWriter.RW;
 
 import java.io.File;
+import java.util.Random;
+
 import ClassSet.Content.UserContent;
 import ClassSet.MFrame.Mframe;
 public class Main{
     public static void main(String[] args){
+
         File userFile = new File("../leagueofjava/content/data/userdata.gdata");
         UserContent user1 = new UserContent(userFile);
         user1.loadUserList();
@@ -15,6 +18,17 @@ public class Main{
         Mframe frame = new Mframe();
         frame.menu();
         frame.mypage();
-
+        /* for(int i =0; i<85; i++){
+            System.out.println("");
+            double randomValue = Math.random();
+            int intValue = (int)(randomValue * 50) + 30;
+            double randomValue2 = Math.random();
+            int intValue2 = (int)(randomValue2 * 50) + 30;
+            double randomValue3 = Math.random();
+            int intValue3 = (int)(randomValue3 * 50) + 30;
+            double randomValue4 = Math.random();
+            int intValue4 = (int)(randomValue4 * 50) + 30;
+            System.out.print("/"+intValue+"/"+intValue2+"/"+intValue3+"/"+intValue4);
+        } */ //data random
     }
 }
