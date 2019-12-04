44f7d0311e8bf0fbd7fc43abf3c3c528d24d3b67
krumaska <okokrt@gmail.com>
2019-12-04 19:05:37 +0900

    Update Main.java frame

--- leagueofjava/Main.java
+++ leagueofjava/Main.java
@@ -2,9 +2,9 @@ import Interface.IFRW;
 import ClassSet.ReadWriter.RW;
 
 import java.io.File;
-
 import ClassSet.Content.UserContent;
-public class Main {
+import ClassSet.MFrame.Mframe;
+public class Main{
     public static void main(String[] args){
         File userFile = new File("../leagueofjava/content/data/userdata.gdata");
         UserContent user1 = new UserContent(userFile);
@@ -12,5 +12,9 @@ public class Main {
         System.out.println(user1.getUserList());
         user1.loadDB();
         System.out.println(user1.getUserData("gold"));
+        Mframe frame = new Mframe();
+        frame.menu();
+        frame.mypage();
+
     }
 }
