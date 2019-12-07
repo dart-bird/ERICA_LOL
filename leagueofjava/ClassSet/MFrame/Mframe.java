c253b6f464c2829ea4b06b05912b7b46c047d163
krumaska <51515055+krumaska@users.noreply.github.com>
2019-12-07 23:43:09 +0900

    shop, game frame added

--- leagueofjava/ClassSet/MFrame/Mframe.java
+++ leagueofjava/ClassSet/MFrame/Mframe.java
@@ -3,6 +3,7 @@ public class Mframe {
     public Mframe() {
     }
     public void menu(){
+        System.out.print("\033[H\033[2J"); //clear console / ubuntu
         System.out.println("*******************************");
         System.out.println("1. My Page 2. Shop Page 3. Game");
         System.out.print("Enter Number:");
@@ -16,6 +17,27 @@ public class Mframe {
         System.out.print(" GOLD:"); //get gold
         System.out.println(" EXP:"); //get exp
         System.out.println("1. Menu");
+        System.out.print("Enter Number:");
+    }
+    public void shop(){
+        System.out.print("\033[H\033[2J"); //clear console / ubuntu
+        System.out.println("*******************************");
+        System.out.println("Welcome to LOL shop!");
+        System.out.println("*******************************");
+        System.out.print("Player"); //get id
+        System.out.println("1. Menu");
+        System.out.print("Enter Number:");
+    }
+    public void game(){
+        System.out.print("\033[H\033[2J"); //clear console / ubuntu
+        System.out.println("*******************************");
+        System.out.println("Game Menu");
+        System.out.println("*******************************");
+        System.out.print("ID:"); //get id
+        System.out.print(" GOLD:"); //get gold
+        System.out.println(" EXP:"); //get exp
+        System.out.print("1. Menu ");
+        System.out.print("2. Start Game\n");
         System.out.print("Enter Number:");
     }
 }
