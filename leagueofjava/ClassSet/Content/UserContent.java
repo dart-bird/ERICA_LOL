222e92fed7a7a791ed681154b1054ec8d66d3800
krumaska <okokrt@gmail.com>
2019-12-14 17:36:55 +0900

    UserContent.java include file obj, reform getUserData, UserDeck

--- leagueofjava/ClassSet/Content/UserContent.java
+++ leagueofjava/ClassSet/Content/UserContent.java
@@ -2,36 +2,47 @@ package ClassSet.Content;
 
 import java.io.File;
 import ClassSet.ReadWriter.RW;
+//import ClassSet.Content.PlayerContent;
 import java.util.*;
 
-public class UserContent extends RW {
-    private String UserList = "";
+public class UserContent extends PlayerContent {
+    private String UserData = "";
+    private String UserDeck = "";
     private HashMap <String, String> userdata = new HashMap <String, String>();
+    private HashMap <String, String> userdeck = new HashMap <String, String>();
     
     private String tmpStr = "";
     private int dataIndex = 0;
-    
-    private String s1 = "";
+    private String dataOfkey = "";
 
-    File file;
-    public UserContent(File file) {
+    public UserContent(File file, File file2, File file3) {
         super(file);
-        this.file = file;
-        UserList = "";
+        UserData = ""; UserDeck = "";
         tmpStr = "";
         dataIndex = 0;
-        loadUserList();
-        loadDB();
+        loadContent(file3); //in PlayerContent
+        loadUserData(file);
+        pushUserData();
+        loadUserDeck(file2);
+        pushUserDeck();
+    }
+    
+
+    private void loadUserData(File file) {
+        RW rwUserData = new RW(file);
+        rwUserData.loadContent(file);
+        UserData = rwUserData.getContent();
     }
 
-    private void loadUserList() {
-        loadContent(file);
-        UserList = getContent();
+    private void loadUserDeck(File file) {
+        RW rwUserDeck = new RW(file);
+        rwUserDeck.loadContent(file);
+        UserDeck = rwUserDeck.getContent();
+        
     }
-
-    private void loadDB() {
-        for(int i=0; i<UserList.length(); i++){
-            if(UserList.charAt(i) == ','){
+    private void pushUserData() {
+        for(int i=0; i<UserData.length(); i++){
+            if(UserData.charAt(i) == ','){
                 switch(dataIndex){
                     case 0: // to recognize id
                         userdata.put("id",tmpStr);
@@ -70,36 +81,39 @@ public class UserContent extends RW {
                         break;
                 }
             }
-            tmpStr += UserList.charAt(i);
+            tmpStr += UserData.charAt(i);
         }
     }
-    
-    public String getUserList() {
-        return UserList;
+    public void pushUserDeck() {
+        userdeck.put("INGAME",UserDeck.substring(0, UserDeck.indexOf('/'))); //'/'를 기준으로 선발 후보를 나뉨.
+        userdeck.put("OUTGAME",UserDeck.substring(UserDeck.indexOf('/')+1));
+        //System.out.println(userdeck.get("OUTGAME"));
+    }
+
+    public String getUserDataList() { //review parsing result
+        return UserData;
+    }
+
+    public String getUserDeckList() { //review parsing result
+        return UserDeck;
     }
 
     public String getUserData(String key){
-        switch (key) {
-            case "id":
-                s1 = userdata.get("id").toString();
-                break;
-        
-            case "pw":
-                s1 = userdata.get("pw").toString();
-                break;
-
-            case "exp":
-                s1 = userdata.get("exp").toString();
-                break;
-            
-            case "gold":
-                s1 = userdata.get("gold").toString();
-                break;
-            
-            case "cards":
-                s1 = userdata.get("cards").toString();
-                break;
-        }
-        return s1;
+        return userdata.get(key).toString();
     }
+    
+    public String getUserDeck(String key){
+        //여기 테스트코드 먹여도 작동안함 ㅋㅋ
+        dataOfkey ="";
+        tmpStr = userdeck.get(key);
+        String data[] = tmpStr.split(",");
+        
+        for(int i=0; i<data.length; i++){
+            if (data[i].length() > 0 && data[i].charAt(data[i].length()-1)=='\n') {
+                data[i] = data[i].substring(0, data[i].length()-1);
+            } //값이 안들어온 이유는 data_deck.gdata 뒤에 \n 이 붙어서 그럼 그걸 삭제 해줌.
+            dataOfkey += getPlayerdata(Integer.parseInt(data[i])) + "\n"; // 선수 목록 편하게 보기 위에 뉴라인 먹임
+        }
+        return dataOfkey;
+    }//아니지 내가 좀 잘못한듯
 }
