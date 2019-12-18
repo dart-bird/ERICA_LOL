cd7b21dfc63d7aafbd2f01e1a0ebb798ad9756be
krumaska <okokrt@gmail.com>
2019-12-19 02:51:21 +0900

    Delete IFRW.java due to split file

--- leagueofjava/Interface/IFRW.java
+++
@@ -1,8 +0,0 @@
-package Interface;
-import java.io.File;
-
-public interface IFRW {
-    abstract String getContent();//상점, 마이페이지, 인게임 정보 가져오는데 활용
-    abstract void writeContent(File file, String inContentString);
-    abstract public void loadContent(File file);
-}
