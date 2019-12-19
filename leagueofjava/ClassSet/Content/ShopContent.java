5d486ac969b8689f136074a381a6ecb115b7d6af
krumaska <okokrt@gmail.com>
2019-12-19 15:14:04 +0900

    ShopContent.java include gold check system

--- leagueofjava/ClassSet/Content/ShopContent.java
+++ leagueofjava/ClassSet/Content/ShopContent.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
+import ClassSet.Content.UserContent;
 import ClassSet.ReadWriter.Writer;
 
 public class ShopContent extends PlayerContent {
@@ -13,11 +14,13 @@ public class ShopContent extends PlayerContent {
     private HashMap <Integer, ArrayList<String>> shopdata = new HashMap <Integer, ArrayList<String>>(); //선수 정보 정리 사전
     private String shopdataStr;
     private int chooseRandomVal;
-    File file2;
-    public ShopContent(File playerdataFile, File wdeckFile) {
+    File file; File file2; File file3;
+    public ShopContent(File playerdataFile, File wdeckFile, File userFile) {
         super(playerdataFile);
         loadContent(playerdataFile);
+        this.file = userFile;
         this.file2 = wdeckFile;
+        this.file3 = playerdataFile;
         shopdataStr = "";
         loadShopData();
         chooseRandomVal = 0;
@@ -33,7 +36,7 @@ public class ShopContent extends PlayerContent {
             shopdata.put(i,list);
             //System.out.println("dd"+list);
         }
-        
+
     }
     public String playerInformation(){
         return "[이름] " + list.get(0).toString()+
@@ -45,16 +48,29 @@ public class ShopContent extends PlayerContent {
         " [정글몹] " + list.get(6).toString();
     }
     public String getShopData() {
-        
+        shopdataStr = playerInformation() + "\n";
+        return shopdataStr;
+    }
+    public boolean purchaseProcessing(int price){
+        boolean success = false;
         Random random = new Random();
         chooseRandomVal = random.nextInt(shopdata.size());
         list = shopdata.get(chooseRandomVal);
-        shopdataStr = playerInformation() + "\n";
-        writeGetShopData(file2, Integer.toString(chooseRandomVal), true);
-        return shopdataStr;
+        UserContent user = new UserContent(file, file2, file3); //데이터 사용자 정보, 선수 데이터, 사용자 덱 로드
+        if(Integer.parseInt(user.getUserData("gold"))>=price) { //골드 500으로 구입조건 제한
+            StringBuffer tmpBuffer = new StringBuffer();
+            tmpBuffer.append(user.getUserData("id"));
+            tmpBuffer.append("," + user.getUserData("pw"));
+            tmpBuffer.append("," + user.getUserData("exp"));
+            tmpBuffer.append("," + (Integer.parseInt(user.getUserData("gold"))-price));
+            user.writeUserContent(file, tmpBuffer.toString(), false); // 골드 정보 저장
+            writeGetShopData(file2, Integer.toString(chooseRandomVal), true);
+            success = true;
+        }
+        return success;
     }
     private void writeGetShopData(File file, String inContentString, boolean append) { // write은 민감하기 때문에 숨김
         Writer wUserData = new Writer(file);
-        wUserData.writeContent(file, "," + inContentString, append); // 형식에 맞춰서 파일작성함.
+        wUserData.writeContent(file, "," + inContentString, append); // 카드 정보 저장
     }
 }
