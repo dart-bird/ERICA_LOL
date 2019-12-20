be206408e82b8d2816ad464660a511344f674e97
krumaska <okokrt@gmail.com>
2019-12-21 03:03:23 +0900

    GameContent.java include BotContent.java

--- leagueofjava/ClassSet/Content/GameContent.java
+++ leagueofjava/ClassSet/Content/GameContent.java
@@ -1,28 +1,76 @@
 package ClassSet.Content;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.lang.reflect.Array;
 
 import ClassSet.Content.UserContent;
+import ClassSet.ReadWriter.Reader;
 import ClassSet.Content.ShopContent;
 import ClassSet.Content.PlayerContent;
-import ClassSet.Content.BotContent;
 
-public class GameContent {
+public class GameContent extends PlayerContent {
 
-    private File botFile; 
-    private File userFile; private File playerFile; private File deckFile;
+    private File botdataFile = new File("../leagueofjava/content/data/botdata.gdata");
+    private File userdataFile = new File("../leagueofjava/content/data/userdata.gdata");
+    private File playerdataFile = new File("../leagueofjava/content/data/userdata.gdata");
+    private File deckdataFile = new File("../leagueofjava/content/data/userdata.gdata");
 
-    public GameContent(File botFile, File userFile, File playerFile, File deckFile) {
+    private HashMap<Integer, ArrayList<String>> botDeckClassfic = new HashMap<Integer, ArrayList<String>>();
+    private HashMap<Integer, ArrayList<String>> userDeckClassfic = new HashMap<Integer, ArrayList<String>>();
+    private String botList;
 
-        this.botFile = botFile;
-        this.userFile = userFile;
-        this.playerFile = playerFile;
-        this.deckFile = deckFile;
-
+    public GameContent(File playerdataFile) throws IOException {
+        super(playerdataFile);
+        botList = "";
+        loadBot();
+        pushBotDeck();
+        loadContent(playerdataFile);
+        loadUserPlayer();
     }
 
-    private BotContent botContent = new BotContent(playerFile, botFile);
-    private UserContent userContent = new UserContent(userFile, playerFile, deckFile);
+    private void loadBot() throws IOException {
+        Reader botReader =  new Reader(botdataFile);
+        botReader.loadContent(botdataFile);
+        botList = botReader.getContent();
+    }
+
+    private void loadUserPlayer() throws IOException {
+        UserContent userContent = new UserContent(userdataFile, deckdataFile, playerdataFile);
+        userDeckClassfic = userContent.getHashMapUserPlayer();
+    }
+
+    private String[] dataIndexFormating(String listStr, String regex){
+        String[] dataIndex = listStr.split(regex);
+        return dataIndex;
+    } 
+    ArrayList<String> list = new ArrayList<String>(); // init list
+    private void pushBotDeck() { // push hashmap with classfied data
+        String[] botListIndex = dataIndexFormating(botList, ",");
+        System.out.println(botList);
+        for (int i = 0; i < botListIndex.length; i++) {
+            String tmpStr = getPlayerdata(Integer.parseInt(botListIndex[i]));
+            list = new ArrayList<String>(); // init list
+            String[] splitbotPlayer = dataIndexFormating(tmpStr, "/");
+            for (int j = 0; j < 7; j++) { // save splited bot player data, index 0 ~ 6
+                list.add(splitbotPlayer[j]);
+            }
+        }
+    }
+    // key: 0 = top, 1 = mid, 2 = dealer, 3 = supporter, 4 = jungler
+    // statInex: 1 = name, 2 = nickname, 3 = team, 4 = skillShot, 5 = roaming, 6 = cs, 7 = jungleTry
+    public String getBotPlayerStat(int key, int statIndex) {
+        list = botDeckClassfic.get(key);
+        System.out.println(list);
+        return list.get(statIndex);
+    }
+    public String getUserPlayerStat(int key, int statIndex) {
+        list = userDeckClassfic.get(key);
+        return list.get(statIndex);
+    }
+
     
     // TODO: 게임에 참가하는 선수들 리스트를 보여주고 거기에 따른 팀의 평균치를 게임 시작 전에 보여줘야함
 }
