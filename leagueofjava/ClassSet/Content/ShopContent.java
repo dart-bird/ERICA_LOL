c469426603c4da03713e96a831c909fc7a445fc9
krumaska <okokrt@gmail.com>
2019-12-21 03:06:35 +0900

    ShopContent.java include HashMap

--- leagueofjava/ClassSet/Content/ShopContent.java
+++ leagueofjava/ClassSet/Content/ShopContent.java
@@ -2,6 +2,7 @@ package ClassSet.Content;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
@@ -10,11 +11,14 @@ import ClassSet.Content.UserContent;
 import ClassSet.ReadWriter.Writer;
 
 public class ShopContent extends PlayerContent {
-    ArrayList<String> list = new ArrayList<String>(); //정보 정리에 필요한 리스트
-    private HashMap <Integer, ArrayList<String>> shopdata = new HashMap <Integer, ArrayList<String>>(); //선수 정보 정리 사전
+    ArrayList<String> list = new ArrayList<String>(); // 정보 정리에 필요한 리스트
+    private HashMap<Integer, ArrayList<String>> shopdata = new HashMap<Integer, ArrayList<String>>(); // 선수 정보 정리 사전
     private String shopdataStr;
     private int chooseRandomVal;
-    File file; File file2; File file3;
+    File file;
+    File file2;
+    File file3;
+
     public ShopContent(File playerdataFile, File wdeckFile, File userFile) {
         super(playerdataFile);
         loadContent(playerdataFile);
@@ -26,32 +30,32 @@ public class ShopContent extends PlayerContent {
         chooseRandomVal = 0;
         // TODO Auto-generated constructor stub
     }
+
     private void loadShopData() {
-        for (int i = 0; i<getPlayerdataCnt(); i++) {
+        for (int i = 0; i < getPlayerdataCnt(); i++) {
             list = new ArrayList<String>();
             String[] splitData = getPlayerdata(i).split("/");
             for (int j = 0; j < 7; j++) { // 선수 하나의 정보 7개를 리스트로 저장
                 list.add(splitData[j]);
             }
-            shopdata.put(i,list);
-            //System.out.println("dd"+list);
+            shopdata.put(i, list);
+            // System.out.println("dd"+list);
         }
 
     }
-    public String playerInformation(){
-        return "[이름] " + list.get(0).toString()+
-        " [닉네임] " + list.get(1).toString()+ 
-        " [팀] " + list.get(2).toString()+ 
-        " [스킬샷] " + list.get(3).toString()+
-        " [로밍능력] " + list.get(4).toString()+
-        " [CS] " + list.get(5).toString()+
-        " [정글몹] " + list.get(6).toString();
+
+    public String playerInformation() {
+        return "[이름] " + list.get(0).toString() + " [닉네임] " + list.get(1).toString() + " [팀] " + list.get(2).toString()
+                + " [스킬샷] " + list.get(3).toString() + " [로밍능력] " + list.get(4).toString() + " [CS] "
+                + list.get(5).toString() + " [정글몹] " + list.get(6).toString();
     }
+
     public String getShopData() {
         shopdataStr = playerInformation() + "\n";
         return shopdataStr;
     }
-    public boolean purchaseProcessing(int price){
+
+    public boolean purchaseProcessing(int price) throws IOException {
         boolean success = false;
         Random random = new Random();
         chooseRandomVal = random.nextInt(shopdata.size());
