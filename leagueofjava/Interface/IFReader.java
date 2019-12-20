cb2203c899682e145630a631cf033b6742f19aaf
krumaska <okokrt@gmail.com>
2019-12-21 03:13:23 +0900

    IFReader.java modified loadcontent part

--- leagueofjava/Interface/IFReader.java
+++ leagueofjava/Interface/IFReader.java
@@ -1,8 +1,9 @@
 package Interface;
 
 import java.io.File;
+import java.io.IOException;
 
 public interface IFReader {
     abstract String getContent();//상점, 마이페이지, 인게임 정보 가져오는데 활용
-    abstract public void loadContent(File file);
+    abstract public void loadContent(File file) throws IOException;
 }
